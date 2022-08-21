13009b26a6099c0b26b0cee400f348734bdd38cb
Petr Volkov <petr.volkov@astrazeneca.com>
2022-08-21 21:50:28 +0200

    implementing org-roam

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,3 +7,16 @@
 (require 'org)
 (org-babel-load-file
  (expand-file-name "config/init.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(citar-org-roam citar yaml-mode xclip which-key vertico use-package projectile pdf-tools pcre2el org-roam org-present org-journal orderless monokai-theme marginalia lsp-ui lsp-jedi key-chord ivy hydra htmlize helm-org helm-bibtex flycheck embark-consult eglot doom-modeline diminish company bibtex-completion)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
