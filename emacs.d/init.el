97a8e6351d8e39b03f810711c1e911fbe340b963
suslik <dunordavind@protonmail.com>
2022-08-31 21:00:08 +0200

    fixing issue with undo keybinding overwritten by org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,16 +9,3 @@
 (require 'org)
 (org-babel-load-file
  (expand-file-name "config/init.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(vundo undo-tree eglot-grammarly yasnippet yaml-mode which-key vertico use-package treemacs-projectile treemacs-icons-dired treemacs-all-the-icons spaceline-all-the-icons snakemake-mode reverse-im restart-emacs pyvenv org-superstar org-roam org-present org-modern org-journal orderless monokai-theme math-symbol-lists marginalia magit-popup magit lsp-ui lsp-treemacs lsp-pyright lsp-jedi lsp-ivy lsp-grammarly julia-mode jedi-core highlight-indentation helm flycheck exec-path-from-shell embark-consult elpher eglot dracula-theme doom-modeline dockerfile-mode diminish counsel citar)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
